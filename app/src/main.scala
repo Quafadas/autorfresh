0dd642b001aa9a89d2746a6c1e382d27dda1f5a6
Simon Parten <quafadas@gmail.com>
2025-09-25 18:21:29 +0200

    .

--- app/src/main.scala
+++ app/src/main.scala
@@ -16,6 +16,7 @@ def main: Unit =
 
 def interactiveApp =
   val hiVar = Var("Update me!")
+  val editorText = Var("Hello from Quill!")
 
   div(
     h1(s"Hello Quill "),
@@ -29,6 +30,17 @@ def interactiveApp =
         val editor = quillMod.default(ctx.thisNode.ref, opt)
 
         editor.setText("Hello from Quill!")
+
+        // Listen for text changes and update the reactive variable
+        val changeHandler: js.Function = () => {
+          val currentText = editor.getText()
+          editorText.set(currentText)
+        }
+        editor.on("text-change", changeHandler)
       }
+    ),
+    p(
+      "Editor content: ",
+      child.text <-- editorText.signal
     )
   )
