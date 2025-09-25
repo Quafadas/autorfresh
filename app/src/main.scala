cafeedf9ad921eda23871c6242d485db3df8fb7b
Simon Parten <quafadas@gmail.com>
2025-09-25 18:17:18 +0200

    This seems to work...

---
+++ app/src/main.scala
@@ -0,0 +1,34 @@
+import org.scalajs.dom
+import org.scalajs.dom.document
+import com.raquo.laminar.api.L.{*, given}
+import scala.scalajs.js
+
+import typings.quill.quillMod
+import typings.quill.coreQuillMod.QuillOptions
+import typings.quill.themesSnowMod
+
+@main
+def main: Unit =
+  renderOnDomContentLoaded(
+    dom.document.getElementById("app"),
+    interactiveApp
+  )
+
+def interactiveApp =
+  val hiVar = Var("Update me!")
+
+  div(
+    h1(s"Hello Quill "),
+    div(
+      idAttr := "editor",
+      styleAttr := "height: 200px; border: 1px solid #ccc;",
+      onMountCallback { ctx =>
+
+        val opt = QuillOptions().setTheme("snow")
+
+        val editor = quillMod.default(ctx.thisNode.ref, opt)
+
+        editor.setText("Hello from Quill!")
+      }
+    )
+  )
